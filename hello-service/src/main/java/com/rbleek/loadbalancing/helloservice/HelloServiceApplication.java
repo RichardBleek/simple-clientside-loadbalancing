b4d33bf4a4afc9841a87bea5ec4a66c52c8a40ec
Richard Bleek <richardbleek@gmail.com>
2020-02-17 12:53:08 +0100

    hello endpoint

--- hello-service/src/main/java/com/rbleek/loadbalancing/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/rbleek/loadbalancing/helloservice/HelloServiceApplication.java
@@ -2,6 +2,14 @@ package com.rbleek.loadbalancing.helloservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
 @SpringBootApplication
 public class HelloServiceApplication {
@@ -10,4 +18,14 @@ public class HelloServiceApplication {
 		SpringApplication.run(HelloServiceApplication.class, args);
 	}
 
+	@Bean
+	public RouterFunction<ServerResponse> route() {
+		return RouterFunctions
+				.route(GET("/hello"),
+						serverRequest -> ok().body(sayHello(), String.class));
+	}
+
+	private Mono<String> sayHello() {
+	    return Mono.just("Hello!");
+	}
 }
