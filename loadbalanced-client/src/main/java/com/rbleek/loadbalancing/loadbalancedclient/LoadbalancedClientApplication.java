1b2da325bf3e284b26b02803abac56f7416b33c4
Richard Bleek <richardbleek@gmail.com>
2020-02-17 13:36:10 +0100

    empty loadbalancing project

---
+++ loadbalanced-client/src/main/java/com/rbleek/loadbalancing/loadbalancedclient/LoadbalancedClientApplication.java
@@ -0,0 +1,13 @@
+package com.rbleek.loadbalancing.loadbalancedclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbalancedClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadbalancedClientApplication.class, args);
+	}
+
+}
---
+++ loadbalanced-client/src/test/java/com/rbleek/loadbalancing/loadbalancedclient/LoadbalancedClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbleek.loadbalancing.loadbalancedclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancedClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
